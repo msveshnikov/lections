07cd62641196c37e9db3806bc1acef4c7c828309
Max Sveshnikov <msveshnikov@gmail.com>
2015-11-14 17:46:00 +0600

    MVP

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,7 +1,18 @@
+# encoding: utf-8
 class ArticlesController < ApplicationController
+  before_action :set_article
+
   def show
+    render file: "/public/material/"+@article.id+".html"
   end
 
-  def index
+  private
+
+  # Use callbacks to share common setup or constraints between actions.
+  def set_article
+    @article = Article.find(params[:id])
+    #@parent = Category.find(@category.ParentCategory_ID) if @category.ParentCategory_ID != 0
+    #add_breadcrumb 'МЕНЮ', :root_path
+    #add_breadcrumb @parent.Title, @parent if @parent
   end
 end
