3a8a0a649f1d69acb5cf507d589593f72615017e
Max Sveshnikov <msveshnikov@gmail.com>
2015-11-18 10:28:25 +0600

    titles

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,15 @@
+# encoding: utf-8
 module ApplicationHelper
+
+  # Returns the full title on a per-page basis.
+  def full_title(page_title)
+    base_title = 'Лекции'
+    if page_title.empty?
+      base_title
+    else
+      "#{base_title} | #{page_title}"
+    end
+  end
 
   def getfavs
     s = cookies[:fav]
