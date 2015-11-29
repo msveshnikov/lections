ab594aa4261059866a94126b4e2e1864d57406e8
Max Sveshnikov <msveshnikov@gmail.com>
2015-11-29 17:14:20 +0600

    fixes

--- app/controllers/ratings_controller.rb
+++ app/controllers/ratings_controller.rb
@@ -2,12 +2,7 @@ class RatingsController < ApplicationController
 
   def update
     @rating = Rating.find(params[:id])
-    @article = @rating.article
-    if @rating.update_attributes(score: params[:score])
-      respond_to do |format|
-        format.js
-      end
-    end
+    @rating.update_attributes(score: params[:score])
   end
 
 end
